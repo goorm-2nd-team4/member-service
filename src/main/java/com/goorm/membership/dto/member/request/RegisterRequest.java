b301e77ac8996e87a44d5e74d04885e206944322
규나다 <qnada0118@gmail.com>
2026-04-14 14:59:06 +0900

    [FIX]문구 수정

--- src/main/java/com/goorm/membership/dto/member/request/RegisterRequest.java
+++ src/main/java/com/goorm/membership/dto/member/request/RegisterRequest.java
@@ -5,17 +5,17 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
 public record RegisterRequest(
-        @NotBlank(message = "이메일은 필수값입니다.")
+        @NotBlank(message = "이메일은 필수입니다.")
         @Email(message = "형식이 올바르지 않습니다.")
         String email,
 
-        @NotBlank(message = "비밀번호는 필수값입니다.")
+        @NotBlank(message = "비밀번호는 필수입니다.")
         String password,
 
-        @NotBlank(message = "비밀번호 확인은 필수값입니다.")
+        @NotBlank(message = "비밀번호 확인은 필수입니다.")
         String confirmPassword,
 
-        @NotBlank(message = "이름은 필수값입니다.")
+        @NotBlank(message = "이름은 필수입니다.")
         String name
 ) {
     @AssertTrue(message = "비밀번호가 일치하지 않습니다.")
